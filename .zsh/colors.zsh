e8a6986c6758915e98ceffbb09b53e12fdff999b
Matthias Hollerbach <matzehollerbach@web.de>
2018-03-17 17:50:45 +0100

    updated theme config

--- .zsh/colors.zsh
+++ .zsh/colors.zsh
@@ -1,1 +1,2 @@
-[[ -d $BASE16_SHELL ]] && source "$BASE16_SHELL/scripts/base16-solarized-dark.sh"
+#[[ -d $BASE16_SHELL ]] && source "$BASE16_SHELL/scripts/base16-solarized-dark.sh"
+[[ -d $BASE16_SHELL ]] && source "$BASE16_SHELL/scripts/base16-onedark.sh"
