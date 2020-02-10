a3cabf396bf9148bc4b8d068a223ee5fb0ab67b1
Matthias Hollerbach <mail@matthias-hollerbach.de>
2020-02-10 18:30:53 +0000

    fix init error

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,9 +2,3 @@
 command -v pyenv >/dev/null && eval "$(pyenv init -)"
 command -v fasd >/dev/null && eval "$(fasd --init auto)"
 
-command -v fzf >/dev/null && {
-  fzf_path="$(dirname $(realpath $(which fzf)))/.."
-  source "$fzf_path/shell/completion.zsh"
-  source "$fzf_path/shell/key-bindings.zsh"
-  unset fzf_path
-}
