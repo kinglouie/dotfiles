978acd8133d1a86d397bb65128dd830269150631
Matthias Hollerbach <matzehollerbach@web.de>
2020-03-26 15:23:34 +0100

    update

--- tag-arch/config/polybar/launch_polybar.sh
+++ tag-arch/config/polybar/launch_polybar.sh
@@ -1,1 +1,12 @@
-/home/mh/projects/personal/dotfiles/config/polybar/launch_polybar.sh
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar & fi
