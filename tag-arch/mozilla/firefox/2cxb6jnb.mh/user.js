039fd70e6c13f46ce13b0120541bbfe9676978de
Matthias Hollerbach <mail@matthias-hollerbach.de>
2020-04-16 14:23:22 +0200

    update

--- tag-arch/mozilla/firefox/2cxb6jnb.mh/user.js
+++ tag-arch/mozilla/firefox/2cxb6jnb.mh/user.js
@@ -1,1 +1,3 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("startup.homepage_welcome_url", "https://google.de");
+user_pref("browser.startup.homepage", "https://google.de");
